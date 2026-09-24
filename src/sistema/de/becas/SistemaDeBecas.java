f98c402f342e914938777d496c78632493076048
Jorge Alfaro <jorge.alfaro.r@mail.pucv.cl>
2026-09-24 10:08:12 -0300

    Probando

--- src/sistema/de/becas/SistemaDeBecas.java
+++ src/sistema/de/becas/SistemaDeBecas.java
@@ -16,6 +16,7 @@ public class SistemaDeBecas {
      */
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        System.out.println("Probando");
     }
     
 }
